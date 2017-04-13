4d483b732820f281e51946b5b0d45f9c22d1f8c7
Ashley Schauer <aelschauer@gmail.com>
2017-04-13 10:42:36 -0600

    add search bar for store search and controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
       resources :items, only: [:index, :show, :create, :update, :destroy]
     end
   end
+
+  resources :search, only: :index
 end
---
+++ spec/features/user_can_search_for_stores_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User can search for stores' do
+  it 'by the zip code' do
+      # Then my current path should be "/search" (ignoring params)
+      # And I should see stores within 25 miles of 80202
+      # And I should see a message that says "16 Total Stores"
+      # And I should see exactly 10 results
+      # And I should see the long name, city, distance, phone number and store type for each of the 10 results
+
+    visit root_path
+
+    binding.pry
+
+    fill_in "search[zip_code]", with: "80202"
+    click_on "Search"
+
+  end
+end
